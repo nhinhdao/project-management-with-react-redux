5ebc15149310ab7574381107fe71fdd36bcf0400
Nhinh Dao <nhinhdao.vn@gmail.com>
2019-03-15 19:16:26 -0400

    Added switch to match route path

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import WelcomePage from './components/WelcomePage';
 import Homepage from './components/Homepage';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import './App.css'
+import { Router, Route, Switch} from 'react-router-dom';
+import './App.css';
+import history from './components/history'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
+        <Router history={history}>
           <Switch>
             <Route exact path="/login" component={WelcomePage} />
             <Route exact path="/" component={Homepage} />
