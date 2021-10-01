42764d3e167bc3db68476adbe6c9d93ec50a2d26
Nhinh Dao <ndao2803@gmail.com>
2021-10-01 09:17:24 -0400

    Fixed wrong time frame being rendered

--- src/components/ProjectTimeline.js
+++ src/components/ProjectTimeline.js
@@ -18,30 +18,42 @@ class ProjectTimeline extends Component {
     const items = [];
 
     this.props.projects.forEach(function(project, index){
-      groups.push({id: index + 1, title: project.title}); 
+      groups.push({id: index + 1, title: project.title});
       items.push({
-        id: project.id, group: index + 1, title: project.description, 
+        id: project.id, group: index + 1, title: project.description,
         start_time: moment(project.start_date), end_time: moment(project.end_date),
         canMove: false, canResize: false, canChangeGroup: false,
         bgColor: randomColor({luminosity: "light", format: "rgba", alpha: 0.8})
       })}
     )
-    
+
+    const timeline = (
+      <Timeline
+        groups={groups}
+        items={items}
+        sidebarContent={<h3>Project</h3>}
+        itemRenderer={this.itemRenderer}
+        itemHeightRatio={0.7}
+        defaultTimeStart={moment()}
+        defaultTimeEnd={moment().add(1, "years")}
+        lineHeight={35}/>
+    )
+
+    const emptyTimeLine = (
+      <h3>Wow, such empty. Please create a project to see it here!</h3>
+    )
+
+    const renderTimeline = () => {
+      return this.props.projects.length == 0 ? emptyTimeLine : timeline;
+    }
+
     return(
       <React.Fragment>
         <div>
           <Button size='small' color='grey'>Projects: {this.props.projects.length}</Button> <Link to={`/newproject`}><Button size='small' color='teal'>Add New Project</Button></Link>
         </div>
         <hr/>
-        <Timeline groups={groups}
-        items={items}
-        sidebarContent={<h3>Project</h3>}
-        itemRenderer={this.itemRenderer}
-        itemHeightRatio={0.7}
-        defaultTimeStart={moment('2019-03-08')}
-        defaultTimeEnd={moment('2019-04-08')}
-        lineHeight={35}
-        />
+        { renderTimeline() }
       </React.Fragment>
     )
   }
